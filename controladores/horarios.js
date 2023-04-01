f49fa606604fffb2b73d4e96806016fc7f12d62d
Jesús López Segura <jlopseg049@g.educaand.es>
2023-04-01 15:44:39 +0200

    CRUD Back (Faltan Pruebas Editar y Borrar)

--- controladores/horarios.js
+++ controladores/horarios.js
@@ -96,6 +96,28 @@ const deleteHorarioById = async (req, res) => {
     }
 };
 
+// Delete DiaHorario
+const deleteDiaHorarioById = async (req, res) => {
+    const diaHorarioId = req.params.id;
+
+    try {
+        // Busca el día de horario a eliminar
+        const diaHorario = await DiaHorario.findById(diaHorarioId);
+
+        if (!diaHorario) {
+            return res.status(404).json({ mensaje: 'Día y horario no encontrado' });
+        }
+
+        // Elimina el día de horario
+        await diaHorario.remove();
+
+        res.status(200).json({ mensaje: 'Día y horario eliminado con éxito' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensaje: 'Error al eliminar el día y horario' });
+    }
+}
+
 // Get Dias Horario By User Id
 const getDiasHorarioByUserId = async (req, res) => {
     const usuarioId = req.params.id; // asumiendo que el id del usuario se recibe como parámetro en la ruta
@@ -132,13 +154,98 @@ const getDiasHorarioByUserId = async (req, res) => {
     }
 };
 
+//Get Dia Horario By Id
+const getDiaHorarioById = async (req, res) => {
+    const diaHorarioId = req.params.id;
+    try {
+        const diaHorario = await DiaHorario.findById(diaHorarioId);
+        if (!diaHorario) {
+            return res.status(404).json({ mensaje: 'Día y horario no encontrado' });
+        }
+        res.status(200).json({ diaHorario });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensaje: 'Error al obtener el día y horario' });
+    }
+}
 
+const editarDiaHorario = async (req, res) => {
+    const { diaSemana, activo, tramoHorario } = req.body;
+    const diaHorarioId = req.params.id;
+    try {
+        const diaHorario = await DiaHorario.findById(diaHorarioId);
+        if (!diaHorario) {
+            return res.status(404).json({ mensaje: 'Día y horario no encontrado' });
+        }
+        diaHorario.diaSemana = diaSemana;
+        diaHorario.activo = activo;
+        diaHorario.tramoHorario = tramoHorario;
+
+        await diaHorario.save();
+
+        res.status(200).json({ mensaje: 'Día y horario actualizado con éxito' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensaje: 'Error al actualizar el día y horario' });
+    }
+}
+//Al editar un horario, se debe editar el horario y los días de horario asociados
+//Ademas si algún dia del antiguo horario no se encuentra en el nuevo horario, se debe eliminar
+//Si algún dia del nuevo horario no se encuentra en el antiguo horario, se debe crear
+const editarHorario = async (req, res) => {
+    const { nombre, dias, tramoHorario } = req.body;
+    const horarioId = req.params.id;
+    try {
+        const horario = await Horario.findById(horarioId);
+        if (!horario) {
+            return res.status(404).json({ mensaje: 'Horario no encontrado' });
+        }
+        horario.nombre = nombre;
+        horario.tramoHorario = tramoHorario;
+
+        // Buscar los días de horario que pertenecen al horario
+        const diasHorario = await DiaHorario.find({ horario: horarioId });
+
+        // Para cada día de horario, si no se encuentra en el nuevo horario, eliminarlo
+        for (let diaHorario of diasHorario) {
+            const diaEncontrado = dias.find((dia) => dia._id === diaHorario._id);
+            if (!diaEncontrado) {
+                await diaHorario.remove();
+            }
+        }
+
+        // Para cada día del nuevo horario, si no se encuentra en el antiguo horario, crearlo
+        for (let dia of dias) {
+            const diaEncontrado = diasHorario.find((diaHorario) => diaHorario._id === dia._id);
+            if (!diaEncontrado) {
+                const nuevoDiaHorario = new DiaHorario({
+                    diaSemana: dia.diaSemana,
+                    activo: dia.activo,
+                    tramoHorario: dia.tramoHorario,
+                    horario: horario._id
+                });
+                await nuevoDiaHorario.save();
+            }
+        }
+
+        await horario.save();
+
+        res.status(200).json({ mensaje: 'Horario actualizado con éxito' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensaje: 'Error al actualizar el horario' });
+    }
+}
 //exportamos las funciones
 module.exports = {
     postHorario,
     postDiaHorario,
     deleteHorarioById,
-    getDiasHorarioByUserId
+    deleteDiaHorarioById,
+    getDiasHorarioByUserId,
+    getDiaHorarioById,
+    editarDiaHorario,
+    editarHorario
 }
 
 
